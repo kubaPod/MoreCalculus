84b8d07922113f2718c77a650be997a81229bc50
Kuba Podkalicki <kuba.pod@gmail.com>
2016-02-27 18:16:57 +0100

    commit

--- Dev/release.m
+++ Dev/release.m
@@ -1,6 +1,20 @@
 (* Mathematica Source File  *)
 (* Created by Mathematica Plugin for IntelliJ IDEA *)
-(* :Author: user *)
+(* :Author: Kuba *)
 (* :Date: 2016-02-23 *)
 
+BeginPackage["MoreCalculus`Private`"]
 
+  releasePath = FileNameJoin[{"E", "Releases", "MoreCalculus"}];
+
+  releaseProject[]:= Module[{},
+    CopyDirectory[
+      FileNameJoin[{ FileNameDrop[$InputFileName,2], "MoreCalculus"}],
+      releasePath,
+      CreateIntermediateDirectories -> True
+    ];
+
+  ];
+
+
+End[]
